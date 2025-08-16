1d1b334944eef65a641da74bde07a229626f6bb8
deepak-kumar-saini2 <876467deepak@gmail.com>
2025-08-16 12:16:49 +0530

    update stopwatch.jsx

--- src/Stopwatch.jsx
+++ src/Stopwatch.jsx
@@ -1,14 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import './app.css'
 
 function Stopwatch() {
+    const [start,setStart] = useState(false)
+    const [time, setTime] =useState(0)
+    const intervalId = useRef(null)
+    const startTime = useRef(0)
+
+    useEffect(()=>{
+        startTime.current = new Date().getTime()
+        if(start){
+            intervalId.current = setInterval(()=>{
+                setTime((new Date().getTime())-startTime.current)
+            },10)
+        }
+    },[start])
+
+    function letStart(){
+        setStart(true)
+    }
+    
+    function letStop(){
+        clearInterval(intervalId.current)
+        setStart(false)
+    }
+    
+    function letReset(){
+        clearInterval(intervalId.current)
+        setStart(false)
+    }
+
     return <>
         <div className="stopwatch">
-            <div className="screen">00:00:00</div>
+            <div className="screen">{`00:00:00`}</div>
             <div className="buttons">
-                <button className="button start-button">Start</button>
-                <button className="button stop-button">Stop</button>
-                <button className="button reset-button">Reset</button>
+                <button className="button start-button" onClick={letStart}>Start</button>
+                <button className="button stop-button" onClick={letStop}>Stop</button>
+                <button className="button reset-button" onClick={letReset}>Reset</button>
             </div>
         </div>
     </>
